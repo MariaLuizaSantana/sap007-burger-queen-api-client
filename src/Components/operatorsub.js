2e47efa0ee5bf7c8f3e753e08698c3a233345e62
MariaLuizaSantana <costamalu123@outlook.com>
2022-05-24 19:20:47 -0300

    estilização do menu

--- src/Components/operatorsub.js
+++ src/Components/operatorsub.js
@@ -1,25 +1,20 @@
 import React, { useState } from 'react'
-import Operator from '../../Components/operator'
+import Operator from './operator'
 
-function Item (){
+function OperatorSub (){
     
     const [counter, setCounter] = useState(0)
 
-    function increment(){
-        console.log('aumenta')
-        setCounter(counter + 1)
-    } 
     function decrement(){
         console.log('diminui')
         setCounter(counter - 1)
     }
     return(
         <>
-            <Operator clickFunction={increment} calculator='+'/>
             <Operator clickFunction={decrement} calculator='-'/>
             <p>Total: {counter}</p>
         </>
     )
 }
 
-export default Item
+export default OperatorSub
